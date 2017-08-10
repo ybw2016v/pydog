b0a1508c8ec93a8bc2ee186d1edc01cae9b91866
yu <ybw2016v@126.com>
2017-08-10 15:04:51 +0800

    NULL

--- hellow.py
+++ hellow.py
@@ -4,8 +4,16 @@ dog = 5
 cat = int (raw_input('Enter an integer : '))
 if dog == cat:
     print 'dog is cat'
+
 elif dog > cat :
     print 'dog is bigger than cat'
 else :
     print 'cat is more'
 print 'end'
+def seekdog():
+    print 'you are a dog'
+    print '''dog
+dog
+dog'''
+seekdog()
+# a dog
