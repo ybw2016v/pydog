0362fbe0f17dd0998d11c8ce733dfe9f701a9d83
yu <ybw2016v@126.com>
2017-08-11 21:08:03 +0800

    something new

--- zhidog.py
+++ zhidog.py
@@ -1,6 +1,24 @@
 #Filename=zhidog.py
-import requests
-r = requests.get("https://www.zhihu.com", headers = \
+import requests,time, os
+#import cookies
+from http import cookiejar
+session = requests.session()
+session.cookies = cookiejar.LWPCookieJar(filename='/home/yu/aadog/cookies.txt')
+try:
+    session.cookies.load(ignore_discard=True)
+except :
+    print ("load cookies failed")
+#######################
+r = requests.get("https://www.zhihu.com/#signin", headers = \
 { 'User-Agent':'ser-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)\
  Ubuntu Chromium/60.0.3112.78 Chrome/60.0.3112.78 Safari/537.36' })
-print r.status_code
+print (r.status_code)
+for name,value in r.headers.items():
+    print ("%s:%s" % (name, value))
+eepdog=r.content#这里的数据类型是bytes，需要用decode转化成字符串str
+#print (eepdog.decode())
+
+
+
+
+session.cookies.save()
